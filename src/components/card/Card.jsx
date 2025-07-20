e6ace83ed8173664a9f3c67038e1060349a4ace2
marcoismakalew <marcoismakalew@gmail.com>
2025-07-20 17:52:31 +0800

    style: card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import Button from "../button/Button";
 
 export default function Card({ cardPict }) {
 	return (
-		<div className="bg-sky-600 rounded-xl lg:w-90 w-70 lg:h-full h-120 shadow-xl/20 lg:m-2 m-auto text-white">
+		<div className="bg-sky-600 rounded-xl lg:w-90 w-70 lg:h-full h-120 shadow-xl/30 lg:m-2 m-auto text-white">
 			<img src={cardPict} alt="" className="w-auto" />
 			<div className="m-6">
 				<h1 className="text-2xl">Title</h1>
